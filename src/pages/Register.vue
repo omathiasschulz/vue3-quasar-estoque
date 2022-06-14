b8e3f20a0976457531a7543e2a717cc7acb58a6a
Mathias Schulz <mathias@schulz.net.br>
2022-06-14 07:44:41 -0300

    feat(register): adiciona botao voltar na pagina

--- src/pages/Register.vue
+++ src/pages/Register.vue
@@ -10,8 +10,10 @@
 
         <q-input label="Senha" v-model="form.password" />
 
-        <div class="full-width q-pt-md">
+        <div class="full-width q-pt-md q-gutter-y-sm">
           <q-btn label="Acessar" color="primary" class="full-width" outline rounded type="submit" />
+
+          <q-btn label="Voltar" color="primary" class="full-width" flat :to="{ name: 'login' }" />
         </div>
       </div>
     </q-form>
